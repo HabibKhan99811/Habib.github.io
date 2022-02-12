ca9571880db8c419ed9f4dc4a0c085c921b087fe
HabibKhan99811 <habibshaukat257@gmail.com>
2022-02-13 00:21:48 +0500

    my porfolio website

---
+++ index.js
@@ -0,0 +1,52 @@
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll',function(){
+  if(window.pageYOffset > 120){
+    nav.classList.add('bg-dark');
+  }else{
+    nav.classList.remove('bg-dark');
+  }
+});
+// active class on nav items
+$(document).ready(function(){
+  $('.nav-link').click(function(){
+      $(this).addClass('active').siblings().removeClass('active');
+  });
+});
+
+//Forms Validation
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+// typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
